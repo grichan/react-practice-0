2ba2067ddf89628346d64540276b51cf412267eb
Vlad <vladislav.grichanliyski@gmail.com>
2021-01-06 22:47:39 +0000

    added transitions to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,21 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {useTransition, animated} from 'react-spring'
 
 function Navigation() {
 
     const [showMenu, setShowMenu] = useState(false)
-
-    let menu
-    
-    if (showMenu) {
-        menu =
-        <div id='nav-list'>
-            AppMenu
-            <ul>
-                <li>Home</li>
-            </ul>
-            <div id="menu-foreground" onClick={() => {setShowMenu(!showMenu)}}></div>
-        </div> 
-    }
-
+    const transitionMenu = useTransition(showMenu, null, {
+        from: { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
+    })
+    const transitionForeground = useTransition(showMenu, null, {
+        from: { position: 'absolute', opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
     
     return (
         <nav>
@@ -30,7 +27,26 @@ function Navigation() {
                     onClick={() => {setShowMenu(!showMenu)}}
                 />
             </div>
-            { menu }
+            {
+                transitionMenu.map(({ item, key, props }) =>
+                item && 
+                <animated.div key={key} style={props} id='nav-list'>
+                    <div>
+                        AppMenu
+                        <ul>
+                            <li>Home</li>
+                        </ul>
+                    </div> 
+                </animated.div>
+                )
+            }
+            {
+                transitionForeground.map(({ item, key, props }) =>
+                item && 
+                <animated.div key={key} style={props} id="menu-foreground" onClick={() => {setShowMenu(!showMenu)}}>
+                </animated.div>
+                )
+            }
         </nav>
     )
 }
